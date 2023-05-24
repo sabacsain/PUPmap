9a2b22301dd3dd1c014d1d1c85aa82726e72cfc4
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2023-05-24 14:18:04 +0800

    ADD - all files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+
+document.querySelector(".close").addEventListener("click", closeModal);
