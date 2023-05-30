ef16484e5d022f843b14e8fe8e3345feb75fd5f2
Mnemosyne-Grayswandir <hamormarygrizelle@gmail.com>
2023-05-30 20:39:25 +0800

    Modified html structure, webpage style, and transition

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function openModal(id) {
   document.getElementById(id).style.display = "block";
+  document.getElementById("photo").style.filter = "grayscale(100%)";
+  document.getElementById("photo").style.opacity = "0.2";
 }
 
 function closeModal(id) {
   document.getElementById(id).style.display = "none";
+  document.getElementById("photo").style.filter = "grayscale(0%)";
+  document.getElementById("photo").style.opacity = "1";
 }
 
 document.querySelector(".close").addEventListener("click", closeModal);
